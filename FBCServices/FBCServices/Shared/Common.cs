6136f0a29f762d7b28576855f2fa964b5b1696ae
TheGrooGetter <j.geiges@gmail.com>
2017-07-24 21:41:31 -0700

    Adding new Projects

---
+++ FBCServices/FBCServices/Shared/Common.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FBCServices.Shared
+{
+    class Common
+    {
+        public static string dbId { get; } = "FBC";
+
+        public static string dbConnectionString => $"{dbId}";
+
+
+    }
+}
